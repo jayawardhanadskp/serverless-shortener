14330a9c875e802379c70f618b83e7741b6742c2
jayawardhanadskp <kasunpradeep.ds@gmail.com>
2025-11-16 06:11:55 -0500

    Rename output variable for API URL and add GitHub Actions role ARN output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
-output "api-url" {
+output "api_url" {
   value = aws_apigatewayv2_api.http_api.api_endpoint
 }
 
@@ -8,4 +8,9 @@ output "cloudfront_url" {
 
 output "dynamodb_table" {
   value = aws_dynamodb_table.urls.name
+}
+
+output "github_actions_role_arn" {
+  value = aws_iam_role.github_actions_role.arn
+  description = "If you prefer to configure GitHub manually, use this role ARN for OIDC"
 }
