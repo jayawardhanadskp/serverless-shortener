335c692a8e652ab6a80af84244bc84f1c135a271
jayawardhanadskp <kasunpradeep.ds@gmail.com>
2025-11-16 10:41:32 -0500

    updated variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,25 +1,24 @@
 variable "aws_region" {
-  description = "AWS region for resources"
-  type        = string
-  default     = "us-east-1"
+  type    = string
+  default = "us-east-1"
 }
 
 variable "domain_name" {
-  description = "Custom domain to use for CloudFront (e.g. short.example.com)"
-  type        = string
+  type    = string
+  default = "short.kasunjayawardhana.com"
 }
 
 variable "hosted_zone_id" {
-  description = "Route53 Hosted Zone ID for domain_name"
-  type        = string
+  type    = string
+  default = "Z06022612792PVIKY9STS"
 }
 
 variable "g_owner" {
-  description = "GitHub owner/org for OIDC trust (e.g. kasun-jayawardhana)"
-  type        = string
+  type    = string
+  default = "jayawardhanadskp"
 }
 
 variable "g_repo" {
-  description = "GitHub repo name for OIDC trust (e.g. serverless-shortener)"
-  type        = string
+  type    = string
+  default = "serverless-shortener"
 }
